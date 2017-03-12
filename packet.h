1103bbb42afad742b34e23e080be7a9dc4387804
Younes Khoudli <younes.khoudli@epita.fr>
2017-03-12 13:32:15 +0100

    wip

---
+++ packet.h
@@ -0,0 +1,23 @@
+
+#define OUSSH_IO_PAYLOAD_SIZE 256
+
+enum oush_packet_type
+{
+  OUSSH_IO,
+  OUSSH_WINDOWS_CHANGE
+}
+
+struct oussh_packet
+{
+  enum oush_packet_type type;
+  struct oush_packet_io
+  {
+    size_t size;
+    char payload[OUSSH_IO_PAYLOAD_SIZE];
+  } io_packet;
+  struct oush_windows_change_packet
+  {
+    struct winsize ws;
+  } windows_change_packet;
+  
+}
